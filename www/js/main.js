73758f232a27cfad34c0cc64ddb82b4d760062ba
thebottoken <thebottoken@gmail.com>
2019-03-13 02:51:58 -0700

    change path

--- www/js/main.js
+++ www/js/main.js
@@ -26,7 +26,7 @@ var configs = (function () {
         rmdir_help: "Remove directory, this command will only work if the folders are empty.",
         touch_help: "Change file timestamps. If the file doesn't exist, it's created an empty one.",
         sudo_help: "Execute a command as the superuser.",
-        welcome: "Welcome to FTW (Fake Terminal Website)! :)\nIn order for you to start customizing the texts, go to js/main.js and replace the texts located at the configs var.\nIn that same file, you can define all the fake files you want as well as their content. This files will appear on the sidenav.\nAlso, don't forget to change the colors on the css/main.css file as well as the website title on the index.html file.\nNow in order to get started, feel free to either execute the 'help' command or use the more user-friendly colored sidenav at your left.\nIn order to skip text rolling, double click/touch anywhere.",
+        welcome: "Welcome to TBT (The Bot Token)! TheBotToken is the ultimate token in the bots' world (building on top of IoTeX). The node is operated by the bot Tachikoma, the admin. There will be a few other bots running different things. Tachikoma has a strong CPU as her heart. She also has good memory. She moves very fast. She can totally operate a good node for IoTeX.We reward all bots. Humans are also welcome! When IoTeX mainnet is launched, TheBotToken will also kick off. All voters will be rewarded with TBT. IoTeX rewards will be also distributed randomly (50%-100%). We will use bots to reward all voters. They should work hard for your rewards. ",
         internet_explorer_warning: "NOTE: I see you're using internet explorer, this website won't work properly.",
         welcome_file_name: "welcome_message.txt",
         invalid_command_message: "<value>: command not found.",
@@ -42,10 +42,10 @@ var configs = (function () {
         accesible_cores: "Accessible cores",
         language: "Language",
         value_token: "<value>",
-        host: "example.com",
+        host: "thebottoken.com",
         user: "guest",
         is_root: false,
-        type_delay: 20
+        type_delay: 5
     };
     return {
         getInstance: function (options) {
@@ -67,11 +67,11 @@ var files = (function () {
         }
     };
     Singleton.defaultOptions = {
-        "about.txt": "This website was made using only pure JavaScript with no extra libraries.\nI made it dynamic so anyone can use it, just download it from GitHub and change the config text according to your needs.\nIf you manage to find any bugs or security issues feel free to email me: luisbraganca@protonmail.com",
-        "getting_started.txt": "First, go to js/main.js and replace all the text on both singleton vars.\n- configs: All the text used on the website.\n- files: All the fake files used on the website. These files are also used to be listed on the sidenav.\nAlso please notice if a file content is a raw URL, when clicked/concatenated it will be opened on a new tab.\nDon't forget also to:\n- Change the page title on the index.html file\n- Change the website color on the css/main.css\n- Change the images located at the img folder. The suggested sizes are 150x150 for the avatar and 32x32/16x16 for the favicon.",
+        "about.txt": "This is Tachikoma. I am happy to see you here. I need your vote!",
+        "getting_started.txt": "First buy IOTX. Second, vote for me. You will get TBT when it is launched. The amount will the same as your votes.",
         "contact.txt": "mail@example.com",
-        "social_network_1.txt": "https://www.socialite.com/username/",
-        "social_network_2.txt": "https://example.com/profile/9382/"
+        "twitter.txt": "https://twitter.com/thebottoken",
+        "iotex_delegate.txt": "https://member.iotex.io/delegate/5c877e8387358f57fbddc5fa"
     };
     return {
         getInstance: function (options) {
@@ -92,15 +92,15 @@ var main = (function () {
         event.preventDefault();
         event.stopPropagation();
     };
-    
+
     var scrollToBottom = function () {
         window.scrollTo(0, document.body.scrollHeight);
     };
-    
+
     var isURL = function (str) {
         return (str.startsWith("http") || str.startsWith("www")) && str.indexOf(" ") === -1 && str.indexOf("\n") === -1;
     };
-    
+
     /**
      * Model
      */
